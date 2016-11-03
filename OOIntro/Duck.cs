9d57a04c1482385dc6e28ac6b865ae0d42a22e02
Savelid <savelid@gmail.com>
2016-11-03 10:55:39 +0100

    Revert "Removed everything :("
    This reverts commit f85157cc0c2123a599aa3bf109eda85dc399a7cf.

---
+++ OOIntro/Duck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOIntro
+{
+    class Duck : Animal
+    {
+        public Duck(string color, double weight) : base(color, weight)
+        {
+        }
+        public Duck() : this("White", 1.1)
+        {
+        }
+        public override string MakeSound()
+        {
+            return "Qwack!";
+        }
+    }
+}
---
+++ OOIntro/IEdible.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOIntro
+{
+    interface IEdible
+    {
+        double Weight { get; set; }
+        void GetEaten();
+    }
+}
