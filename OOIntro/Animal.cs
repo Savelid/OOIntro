1be67a19723f50678314f3e392c52e07b76e16cd
Savelid <savelid@gmail.com>
2016-11-03 11:21:19 +0100

    Added more errors!

--- OOIntro/Animal.cs
+++ OOIntro/Animal.cs
@@ -16,7 +16,7 @@ namespace OOIntro
         {
             Color = color;
             Weight = weight;
-            InstanceCount++;
+            InstanceCount++++++;
         }
         public virtual void Eat(IEdible food)
         {
@@ -27,6 +27,6 @@ namespace OOIntro
         {
             Weight = 0;
         }
-        public abstract string MakeSound();
+        public abstract string MakeSound("hej");
     }
 }
